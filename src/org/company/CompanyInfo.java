22400b629fc631a4a0898a634feddead2bec48d4
vijay-Jayakumar <vijayakumar.mech122@gmail.com>
2022-07-22 19:10:22 +0530

    code added

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -3,26 +3,32 @@ package org.company;
 public class CompanyInfo {
 
 	private void companyName() {
-System.out.println("companyName: M&M company");
+		System.out.println("companyName: M&M company");
 
 	}
+
 	private void companyId() {
-System.out.println("companyId: 158687");
+		System.out.println("companyId: 158687");
 
 	}
+
 	private void companyAddress() {
-System.out.println("companyAddress : 17/18, Pattullous Road, Royapettah");
+		System.out.println("companyAddress : 17/18, Pattullous Road, Royapettah");
 
 	}
+
+	private void companyBranches() {
+		System.out.println("companyBranches:Chennai,Coimbatore");
+	}
+
 	public static void main(String[] args) {
-		//className objectRefName = new ClassName;
-		CompanyInfo v=new CompanyInfo();
-		//objectRefName.methodName;
+		// className objectRefName = new ClassName;
+		CompanyInfo v = new CompanyInfo();
+		// objectRefName.methodName;
 		v.companyName();
 		v.companyAddress();
 		v.companyId();
+		v.companyBranches();
 	}
-	
-	
-	
+
 }
